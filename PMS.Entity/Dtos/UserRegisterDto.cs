2b065abc70b0c0fd4152ef55553aba9b4c961960
Gökdeniz Genç <fbgokdeniz@gmail.com>
2024-07-24 08:56:37 +0300

    changed register role system

--- PMS.Entity/Dtos/UserRegisterDto.cs
+++ PMS.Entity/Dtos/UserRegisterDto.cs
@@ -15,6 +15,5 @@ namespace PMS.Entity.Dtos
         public DateTime BirthDate { get; set; }
         public string Phone { get; set; }
         public string Password { get; set; }
-        public string role {  get; set; }
     }
 }
