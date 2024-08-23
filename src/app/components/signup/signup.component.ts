35fa0108f319075ccc50e1abded87dbf51bdd2ce
hudanurcan <145607810+hudanurcan@users.noreply.github.com>
2024-08-23 16:50:31 +0300

    Update signup.component.ts

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { AuthServiceService } from '../../services/auth.service.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { CommonModule } from '@angular/common';
 import { Register } from '../../model/UserAuth/register';
+import { mailValidator, passwordValidator, phoneValidator, usernameValidator } from '../../validators/validation';
 
 @Component({
   selector: 'app-signup',
@@ -17,6 +18,8 @@ import { Register } from '../../model/UserAuth/register';
 export class SignupComponent implements OnInit {
   userRegisterForm!: FormGroup;
   usereg!:Register;
+
+  showErrorMessage: boolean = false;
   constructor(
     private authService: AuthServiceService,
     private formBuilder: FormBuilder,
@@ -24,21 +27,30 @@ export class SignupComponent implements OnInit {
     private toastrService: ToastrService,
  
   ) {}
+
+  maxDate = "2002-01-01";
+
+  preventKeyboardInput(event: KeyboardEvent): void {
+    event.preventDefault(); // Klavye girişini engeller
+  }
+
   ngOnInit(): void {
     this.authService.checkloginin();
     this.createUserRegisterForm();
   }
   createUserRegisterForm() { 
     this.userRegisterForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-      phone:['', Validators.required],
+      name: ['',[Validators.required, Validators.minLength(3)]],
+      email: ['',  [Validators.required, mailValidator]],
+      username: ['', [Validators.required, usernameValidator]],
+      password: ['', [Validators.required, passwordValidator,Validators.minLength(6)]],
+      phone:['', [Validators.required, phoneValidator]],
       birthDate:['', Validators.required],
       teamid:['', Validators.required],
     });
   }
+
+
   navigateToAddressPage() {
     if (!this.usereg) {
       this.usereg = new Register();
@@ -59,6 +71,31 @@ export class SignupComponent implements OnInit {
       this.router.navigate(['signadresspage']);
     }
    
+  }
+
+
+  onSubmit () {
+    if (this.userRegisterForm.invalid) {
+     // this.showErrorMessage = true; // Hata mesajını göstermek için değişkeni true yapıyoruz
+      alert("HATA"); // Formun başına kaydırır
+      this.toastrService.error('Lütfen tüm alanları doğru bir şekilde doldurunuz!', 'Başarısız');
+      return; // Form geçersiz olduğunda işlemi durdurur
+    }
+
+    this.showErrorMessage = false; // Form geçerliyse hata mesajını gizler
+
+    if (!this.usereg) {
+      this.usereg = new Register();
+    }
+    this.usereg.name = this.userRegisterForm.get('name')?.value;
+    this.usereg.email = this.userRegisterForm.get('email')?.value;
+    this.usereg.username = this.userRegisterForm.get('username')?.value;
+    this.usereg.password = this.userRegisterForm.get('password')?.value;
+    this.usereg.phone = this.userRegisterForm.get('phone')?.value;
+    this.usereg.birthDate = this.userRegisterForm.get('date')?.value;
+
+    this.authService.setMyClassInstance(this.usereg);
+    this.router.navigate(['signadresspage']);
   }
 
 }
